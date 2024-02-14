d9158a78f9e5606ed478d2f55da854a6c6212ba6
AidenKooG <k037686@gmail.com>
2024-02-14 11:40:20 +0900

    feat: Update command type

--- mobile/android/websocket/app/src/main/java/io/github/aidenkoog/websocket/websocket/request/command/CommandType.kt
+++ mobile/android/websocket/app/src/main/java/io/github/aidenkoog/websocket/websocket/request/command/CommandType.kt
@@ -3,12 +3,6 @@ package io.github.aidenkoog.websocket.websocket.request.command
 enum class CommandType {
     CLEAN,
     DEVICE_STATUS,
-    ROBOT_SETTING_DATA,
-    SEND_ROBOT_STATUS,
-    MOTION,
     ON_OFF,
-    TEST_START, TEST, TEST_FINISH,
-    OPERATION_SETTING_DATA,
-    JOINT_TEMP,
-    ICECREAM_COMPLETE,
+    TEST
 }
