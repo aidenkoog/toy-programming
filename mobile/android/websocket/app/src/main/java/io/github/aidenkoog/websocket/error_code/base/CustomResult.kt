452ba60561184ae9654ca7398f9e0a095fd787e9
AidenKooG <k037686@gmail.com>
2024-02-17 23:35:55 +0900

    feat: delete error code package in websocket project

--- mobile/android/websocket/app/src/main/java/io/github/aidenkoog/websocket/error_code/base/CustomResult.kt
+++
@@ -1,6 +0,0 @@
-package io.github.aidenkoog.websocket.error_code.base
-
-sealed class CustomResult<out T> {
-    data class Success<out T>(val value: T) : CustomResult<T>()
-    data class Failure(val exception: Exception) : CustomResult<Nothing>()
-}
