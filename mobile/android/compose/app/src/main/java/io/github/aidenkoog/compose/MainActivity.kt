1bfeb16c6a13dcfb7ee92f4a17510ba6de755408
AidenKooG <k037686@gmail.com>
2024-02-19 20:23:55 +0900

    feat: add example with remember of compose in android

--- mobile/android/compose/app/src/main/java/io/github/aidenkoog/compose/MainActivity.kt
+++ mobile/android/compose/app/src/main/java/io/github/aidenkoog/compose/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,9 +39,66 @@ class MainActivity : ComponentActivity() {
                             Text(text = "Tester2")
                         }
                         AddTexts()
+                        CustomColumnText()
+                        TextViewWithBtn()
+                        ButtonWithRemember()
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun ButtonWithRemember() {
+    val expanded = remember { mutableStateOf(false) }
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(30.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text("Hello")
+                Text("Tester 2")
+            }
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show" else "Hidden")
+            }
+        }
+    }
+}
+
+@Composable
+private fun TextViewWithBtn() {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello")
+                Text(text = "Tester 1")
+            }
+            ElevatedButton(onClick = {}) {
+                Text("CLICK")
+            }
+        }
+    }
+}
+
+@Composable
+private fun CustomColumnText() {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp)
+        ) {
+            Text(text = "TEST 111111")
+            Text(text = "TEST 222222")
         }
     }
 }
